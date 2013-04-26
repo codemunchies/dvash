150ce2813aca003d42b5da0b72ea58129c7aa6b6
Ari Mizrahi <codemunchies@gmail.com>
2013-04-26 18:27:50 -0400

    added compatibility for windows7+

--- lib/dvash/os/windows.rb
+++ lib/dvash/os/windows.rb
@@ -1,10 +1,11 @@
 module Dvash
 	
-	class Windows
+	class Windows < Validation
 
 		def block_ip(address)
 
-			# TODO
+			# win7+ compatible, easier to blackhole the IP
+			system("route add #{address} mask 255.255.255.255 10.255.255.255 if 1 -p")
 		end
 
 	end
